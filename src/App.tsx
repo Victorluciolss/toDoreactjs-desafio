93b91bef7769c405a31b3b7f43e9f9b6f1346807
Victor Lucio <victorlucio@MacBook-Pro-de-Lucas.local>
2022-08-18 21:41:08 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Header } from './components/Header';
+import './styles.css';
+
+export function App() {
+  return (
+    <div>
+      <Header />
+      <strong>vitae</strong>
+    </div>
+  )
+}
+
+
